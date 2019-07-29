66a469280ec8bc328a5f7b5403a64380317ffea7
iamtunde <yusuphtunday@yahoo.com>
2019-07-29 06:45:21 +0100

    Modified the product validator name in the route file

--- app/routes/product.route.js
+++ app/routes/product.route.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const route = express.Router()
-const Validator = require('../middleware/validators/products')
+const productValidator = require('../middleware/validators/products')
 
 //import the product controller
 const productController = require('../controllers/productController')
 
 route.get('/', productController.fetchAll)
-route.post('/create', Validator.create, productController.create)
+route.post('/create', productValidator.create, productController.create)
 route.get('/:id', productController.fetchOne)
 route.put('/:id/update', productController.update)
 route.delete('/:id/delete', productController.delete)
