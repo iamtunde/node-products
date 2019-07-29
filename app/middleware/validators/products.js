920781bfae0031e82d09322239ae328143095eb9
iamtunde <yusuphtunday@yahoo.com>
2019-07-29 06:44:03 +0100

    Included validation with middleware using Joi validation package.

---
+++ app/middleware/validators/products.js
@@ -0,0 +1,18 @@
+const Joi = require('joi')
+
+exports.create = (req, res, next) => {
+    const rules = Joi.object().keys({
+        name : Joi.string().required().max(50),
+        price : Joi.number().required().max(1500)
+    })
+
+    Joi.validate(req.body, rules, (err, result) => {
+        if(err) {
+            return res.status(422).json({
+                message: err.message
+            })
+        }
+    })
+
+    next();
+}
--- app/routes/product.route.js
+++ app/routes/product.route.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const route = express.Router()
+const Validator = require('../middleware/validators/products')
 
 //import the product controller
 const productController = require('../controllers/productController')
 
 route.get('/', productController.fetchAll)
-route.post('/create', productController.create)
+route.post('/create', Validator.create, productController.create)
 route.get('/:id', productController.fetchOne)
 route.put('/:id/update', productController.update)
 route.delete('/:id/delete', productController.delete)
